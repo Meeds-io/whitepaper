251eab29d014829be4b0b68b93c4375cfa14596c
Patrice Lamarque <patrice.lamarque@gmail.com>
2022-06-02 23:00:03 +0200

    attempt to use ... as separator for YML metadata file

--- build.sh
+++ build.sh
@@ -21,27 +21,26 @@ fi
 
 YAML=$CONTENT_PATH/title.yml
 if [[ -f "$YAML" ]]; then
-    echo "Params loaded from $YAML"
-
-    ## workaround Crowdin stripping out --- at end of yaml
+    echo "Pandoc Metadata loaded from $YAML"
+    ## workaround Crowdin stripping out ... at end of yaml
     LL=`tail -n 1 $YAML`
-    if [  "$LL" != "---" ]; then
-        echo "Detected missing --- in metadata yaml. Appending... $LL"
-        echo "\n---" >> $YAML
+    if [  "$LL" != "..." ]; then
+        echo "...\n" >> $YAML
+        echo "WARN : Detected and fixed missing ... at the end of $YAML."
     fi
 else
-    echo "warning : $YAML not found"
+    echo "WARN : Pandoc Metadata missing at $YAML"
 fi
 
-
-
-MDOWN=$CONTENT_PATH/*.md
-echo "Processing content from $MDOWN"
 
 ## cleanup output folder
 mkdir -p build
 rm -rf ./build/*
 OUTPUT=build/meeds-whitepaper-$LANG.pdf
+
+
+MDOWN=$CONTENT_PATH/*.md
+echo "Processing markdown files from $MDOWN"
 
 ## transform all markdown fragments in content folder to pdf using  eisvogel LateX template
 pandoc --pdf-engine=xelatex -f markdown+smart -o $OUTPUT --top-level-division=chapter --template=templates/eisvogel.tex --toc  -s $YAML $MDOWN
